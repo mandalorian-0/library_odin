138252328bf7730e0029c25c3a73d632f179b22d
Rodelin J Cesar <cesarrodelin@outlook.com>
2026-03-14 22:51:54 -0700

    Implement Book constructor and book function create

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,19 @@
-(title, author, pages, haveRead);
+// (title, author, pages, haveRead);
+const books = [];
+
+function Book(title, author, pages, haveRead = false) {
+  if (!new.target) {
+    throw Error("You must use the new keyword when creating a new book");
+  }
+  ((this.id = crypto.randomUUID()),
+    (this.title = title),
+    (this.author = author),
+    (this.pages = pages),
+    (this.have = haveRead));
+}
+
+function createBook(title, author, pages, haveRead) {
+  const new_book = new Book(title, author, pages, haveRead);
+
+  books.push(new_book);
+}
